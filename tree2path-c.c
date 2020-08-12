e7044b6a3a24c12daf21f88c06ccb6250d8a7df3
wwic3478 <3478243770@qq.com>
2020-08-12 20:16:11 +0800

    软件进行函数式开发，重写代码， 目前已完成文件名/文件夹名获取

--- tree2path-c.c
+++ tree2path-c.c
@@ -9,25 +9,7 @@
 #define printffile()	write(destfile,file,file_len)
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 /*
-ANSI文件编码，实现树形目录与文件夹目录的转换
-├── /
-│   ├── KTV/
-│   │   ├── DVD歌库【26.4T-19.2万】/
-│   │   │   ├── 14-15年部分更新/
-│   │   │   │   ├── 14.07/
-│   │   │   │   │   ├── 3秒乐团-ALL-国语-970889.mpg
-│   │   │   │   │   ├── A SHOW-谁说我不在乎你人-国语-970684.mpg
-│   │   │   │   │   ├── A.SA 乐团-过客角色-国语-970891.mpg
-
-/t/
-/t/KTV/
-/t/KTV/DVD歌库【26.4T-19.2万】/
-/t/KTV/DVD歌库【26.4T-19.2万】/14-15年部分更新/
-/t/KTV/DVD歌库【26.4T-19.2万】/14-15年部分更新/14.07/
-/t/KTV/DVD歌库【26.4T-19.2万】/14-15年部分更新/14.07/3秒乐团-ALL-国语-970889.mpg
-/t/KTV/DVD歌库【26.4T-19.2万】/14-15年部分更新/14.07/A SHOW-谁说我不在乎你人-国语-970684.mpg
-/t/KTV/DVD歌库【26.4T-19.2万】/14-15年部分更新/14.07/A.SA 乐团-过客角色-国语-970891.mpg
-/t/KTV/DVD歌库【26.4T-19.2万】/14-15年部分更新/14.07/A.SA 乐团-为爱活一遍-国语-970890.mpg
+ANSI文件编码，实现树形目录与文件路径的转换
 */
 
 
@@ -44,16 +26,18 @@ char line_old[MAX_ASCII];
 char line[MAX_ASCII];
 char path[MAX_ASCII] = "/";
 char file[MAX_ASCII];
+char name[MAX_ASCII]; 
 
-void path_add(char* line ,int len)
+//将路径拼接起来
+//未完成
+void path_add(char* dir_name ,int len)
 {
-	while(line[len] != '\n')
-		path[path_len++] = line[len++];
+	strcat(path,dir_name);
 	//printf("path+:%s+%d+%d+\n",path,path_len,strlen(path));
 } 
 
-void path_del()//	/0214/	6
-{//		/0214/20190116-音/	18
+void path_del()
+{
 	--path_len;
 	path[path_len] = '\0';
 	while(path[--path_len] != '/')
@@ -70,92 +54,104 @@ void path_del()//	/0214/	6
 	return ;
 }
 
+//"├",169,192
+//"│",169,166
+//"└",169,184
+//"─",169,164
+//获取文件、文件夹名字，已完成。
+void get_name(unsigned char* _line,unsigned char* _name)
+{
+	int dir_flag = 0;
+	int space_cnt = 0; 
+	//过度掉前面的字符 
+	while(*_line != '\0')
+	{
+		//不具有文件夹属性 
+		if( ((*_line == 169) && *(_line+1) == 166)	//"│",169,166 
+		 || ((*_line == 169) && *(_line+1) == 164))	//"─",169,164
+		 {
+		 	_line += 2;
+		 }
+		 //具有文件夹属性 
+	 	else if(	((*_line == 169) && *(_line+1) == 192)	//"├",169,192 
+		 ||	((*_line == 169) && *(_line+1) == 184))	//"└",169,184 
+		 {
+		 	dir_flag = 1;
+			_line += 2;
+		 }
+		 else if((*_line ==  32))
+		 {
+		 	++_line;
+		 	++space_cnt;
+		 }
+		 else
+		 {
+		 	//前面只能出现 "│""─"/"├""└"" "这些字符，否则认为前置字符结束 
+		 	break;
+		 }
+	}
+	//printf("space_cnt:%d\t",space_cnt);
+	//printf("cnt :%d\n",cnt);
+	//printf("%d,%d,%d,%d,%d,%\n",*line,*(line+1),*(line+2),*(line+3),*(line+4),*(line+5));
+	//前置字符结束后，遇到换行、文件夹标识符、结束符，就认为名字复制完成。
+	while( *_line != '\n' 
+		&& *_line != '/'	
+		&& *_line != '\0' )
+	{
+		*_name++ = *_line++;
+	}
+	if(dir_flag == 1)
+	{
+		*_name++ = '/';
+	}
+} 
+
+//获取当前文件夹层数
+//未完成
+unsigned char get_dir_cnt(unsigned char* _line)
+{
+	int _dir_cnt = 0;
+	while(*_line != '\0')
+	{
+		if(	((*_line == 169) && *(_line+1) == 192)	//"├",169,192 
+		 || ((*_line == 169) && *(_line+1) == 166))	//"│",-87,-90 
+		 {
+			++_dir_cnt;
+		 }
+		 ++_line;
+	}
+	return _dir_cnt;
+}
+
+//每一行进行处理
+//未完成。
 int calc_line(char* line)
 {
 	int len_dis = 2;
-	int old = 0;
-	int name_dis = 0;
-	static int name_dis_old = 0;
-	int dir_in = 0;
-	dir_cnt = 0;
-	line_len = strlen(line);
-	//前面处理路径问题 
-	while(1)
-	{
-		if(line[len_dis-2] == '\n' || (line[len_dis - 2] == -87 && line[len_dis - 1] == -92))//"─"
-			break; 
-		if(line[len_dis-2] == -87 && line[len_dis-1] == -90)//│符号
-		{
-			dir_cnt ++ ;
-		}
-		len_dis++;
-	}
-
-	if((line[0] == -87 && line[1] == -64)	//"├"，-87，-64 
-		 || (line[0] == -87 && line[1] == -90))//│符号
-	{
-		//再次处理文件名/文件夹名问题 
-		while(line[++len_dis] != '\n')
-		{
-			if((line[len_dis - 3] == -87 && line[len_dis - 2] == -92) && line[len_dis - 1] == ' ')
-			{//"─" + 空格符号，表明遇到了文件夹名称或者文件名称 
-				name_dis = len_dis;//名称首字母下标下标 
-				//接下来对文件名或者文件夹名进行复制 
-				if(dir_cnt < dir_cnt_old)//这层比上层浅 
-				{
-					int out_cnt = dir_cnt_old - dir_cnt;
-					while(out_cnt--)
-						path_del();
-				}
-				else if(dir_cnt > dir_cnt_old)// 这层比上层深 
-				{
-				 	path_add(line_old,name_dis_old);//加长路径 [字符串起始地址，下标]
-				} 
-				dir_cnt_old = dir_cnt;//赋值 
-				
-				file_len = 0;
-				while(line[len_dis] != '\n')
-					file[file_len++] = line[len_dis++];
-				file[file_len] = '\0';
-				printpath();
-				printfile();
-				
-				break;//直接结束，不可能有别的符号了 
-			}
-		}
-		//printf("%d",dir_cnt);
-		//printpath();
-		name_dis_old = name_dis;
-		strcpy(line_old,line);
-	}
-	else
-	{
-		printf("line_error:%d.\n",line_cnt);
-		++error_cnt;
-		return -1; 
-	}
 	return len_dis;
 }
 
 int main(int argc, char *argv[])
 {
 	char* ret;
-	srcfile = fopen(argv[1],"r");//只读方式打开tree文件
-//	destfile = fopen("2019-.txt","w+");//只写方式打开文件
-	//int offset = 0;
-	//while(offset -- )
-	{
-	//	fgets(line,MAX_ASCII,srcfile); 
-	}
+	//srcfile = fopen(argv[1],"r");//只读方式打开tree文件
+	srcfile = fopen("./src.txt","r");
+	//destfile = fopen("2019-.txt","w+");//只写方式打开文件
 	while(!feof(srcfile))
 	{
-		memset(line,strlen(line),0);
+		memset(line,0,strlen(line));
+		memset(name,0,strlen(name));
 		ret = fgets(line,MAX_ASCII,srcfile); 
 		if(ret == NULL)//遇到了文件末尾，跳出循环 
-			break;
+		break;
 		line_cnt ++;
 		//printf("%d,%d:",line[0],line[1]);
-		calc_line(line);
+		get_name(line,name);
+		//dir_cnt = get_dir_cnt(line);
+		//printf("%d\n",dir_cnt);
+		//printf("line:%s",line);
+		printf("%s\n",name);
+		//calc_line(line);
 	}
 	fclose(srcfile);
 	//fclose(destfile);
