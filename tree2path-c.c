9a1f8e1f2af4a5110171ad6a5b0c6b6e2b6f2267
3478243770@qq.com <3478github>
2021-04-29 11:19:14 +0800

    1.使用文件夹分隔符'/' 2.增加输出文件dstfile 3.指针变量的*靠近变量名 4.尽量使用unsigned char * 5.进行强行转换 6.对空行进行处理：就是不做处理 7.增加log文件，记录一些其他信息

--- tree2path-c.c
+++ tree2path-c.c
@@ -13,6 +13,8 @@
 #define HORIZONTAL_SYMBOL "─"
 #define BRANCH_SYMBOL "├"
 #define BROKEN_SYMBOL "└"
+#define DIR_SYMBOL '/'
+//但是数据不一定是正常的。
 //每进入或出来一个文件夹时，改变的前面的符号的个数，Windows有4个，linux有3个
 #define WINDOWS_SYMBOL_CNT_PER_CHANGE_DIR (4)
 #define LINUX_SYMBOL_CNT_PER_CHANGE_DIR (3)
@@ -29,15 +31,19 @@ ANSI文件编码，实现树形目录与文件路径的转换
 */
 
 FILE *srcfile = NULL;
-//FILE *destfile = NULL;
+FILE *destfile = NULL;
+unsigned char srcfile_str[50] = {0};
+unsigned char destfile_str[50] = {0};
 int line_cnt = 0;
-int error_line_cnt = 1;
+int error_line_cnt = 0;
 int error_flag = 0;
+int symbol_cnt = 0;
 int dir_cnt = 0;
 int dir_cnt_old = 1;
 unsigned char line[MAX_ASCII];
-unsigned char path[MAX_ASCII] = "/";
+unsigned char path[MAX_ASCII] = "\0";
 unsigned char name[MAX_ASCII];
+unsigned char buffer[MAX_ASCII];
 unsigned char name_old[MAX_ASCII];
 
 unsigned int symbol_cnt_per_change_dir = WINDOWS_SYMBOL_CNT_PER_CHANGE_DIR;
@@ -53,21 +59,21 @@ unsigned int symbol_cnt_per_change_dir = WINDOWS_SYMBOL_CNT_PER_CHANGE_DIR;
 //}line_info,line_old_info;
 
 //将路径拼接起来
-void path_add(unsigned char* dir_name ,int len)
+void path_add(unsigned char *dir_name ,int len)
 {
-  if (dir_name[strlen(dir_name)-1] != '/')
+  if (dir_name[strlen((char *)dir_name)-1] != DIR_SYMBOL)
   {
-    dir_name[strlen(dir_name)] = '/';
+    dir_name[strlen((char *)dir_name)] = DIR_SYMBOL;
   }
-	strcat(path,dir_name);
+	strcat((char *)path,(char *)dir_name);
 	//printf("path+:%s+%d+%d+\n",path,path_len,strlen(path));
 } 
 
 void path_del()
 {
   //printf("path:%s,",path);
-  int len = strlen(path) - 1;
-	while(path[--len] != '/')
+  int len = strlen((char *)path) - 1;
+	while(path[--len] != DIR_SYMBOL)
 	{
 		if(len <= 0)
 		{
@@ -89,7 +95,7 @@ void dir_cnt_deal()
     if(dir_cnt < dir_cnt_old)
     {
       int diff = dir_cnt_old - dir_cnt;
-      while(diff--)
+      while((diff--) > 0)
       {
         path_del();
       }
@@ -97,11 +103,11 @@ void dir_cnt_deal()
     dir_cnt_old = dir_cnt;
 }
 
-int get_line_info(unsigned char* _line)
+int get_line_info(unsigned char *_line)
 {
-  int symbol_cnt = 0;
-  char* _name = name;
+  unsigned char *_name = name;
   //过渡掉前面的字符
+  symbol_cnt = 0;
   while(*_line != '\0')
   {
     if(((*_line == 169) && *(_line+1) == 166)	//"│",169,166
@@ -126,83 +132,118 @@ int get_line_info(unsigned char* _line)
   //dir_cnt = (symbol_cnt-7)/5;
   //windows
   //dir_cnt = symbol_cnt / 4;
-  dir_cnt = symbol_cnt / symbol_cnt_per_change_dir;
-  printf("space_cnt:%d\t",symbol_cnt);
+  //symbol_cnt -= 2;
+  // 如果出现空行，则不出现故障
+  if (symbol_cnt / symbol_cnt_per_change_dir != 0)
+	  dir_cnt = symbol_cnt / symbol_cnt_per_change_dir;
+  //printf("space_cnt:%d\t",symbol_cnt);
   //printf("%s",_line);
   //printf("cnt :%d\n",cnt);
   //printf("%d,%d,%d,%d,%d,%\n",*line,*(line+1),*(line+2),*(line+3),*(line+4),*(line+5));
   //前置字符结束后，遇到换行、文件夹标识符、结束符，就认为名字复制完成。
-  while( *_line != '\n'
-  // && *_line != '/'
-   && *_line != '\0' )
+  if (dir_cnt != 0)
   {
-    *_name++ = *_line++;
+	  while( *_line != '\n'
+	  // && *_line != '/'
+	   && *_line != '\0' )
+	  {
+		*_name++ = *_line++;
+	  }
   }
 }
 
 
 //每一行进行处理
 //未完成。
-int line_deal(unsigned char* _line)
+int line_deal(unsigned char *_line)
 {
     line_cnt ++;
     //memcpy(&(line_info.data),_line,sizeof(_line));
     //get_name(_line,name);
     get_line_info(_line);
-    printf("%d  ",dir_cnt);
+    //printf("%d  ",dir_cnt);
     dir_cnt_deal();
-
-    printf("%s",path);
-    printf("%s",name);
-    //printf("%s",name_old);
-    //printf("line:%s",line);
-    printf("\n");
-    //calc_line(line);
 }
 
 
-int file_deal(char* file)
+int file_deal(unsigned char *_srcfile,unsigned char *_destfile)
 {
-  char* ret;
-  srcfile = fopen(file,"r");//只读方式打开tree文件
-  //srcfile = fopen("src-f.txt","r");
-  //srcfile = fopen("src-bpcs.txt","r");
-  //srcfile = fopen("./t2.txt","r");
-  //destfile = fopen("2019-.txt","w+");//只写方式打开文件
+  char *ret;
+  FILE* logfile = NULL;
+  if (NULL == _srcfile)
+  {
+	return -1;
+  }
+  srcfile = fopen((char *)_srcfile,"r");//只读方式打开tree文件，如果没有就打开失败。
+  destfile = fopen((char *)_destfile,"w+");//只写方式打开文件，如果没有文件就创建文件。
+  logfile = fopen("tree2path_log.txt","w+");
+  if (NULL == srcfile || NULL == destfile || NULL == logfile)
+  {
+	return -1;
+  }
   while(!feof(srcfile))
   {
     memcpy(name_old,name,sizeof(name));
-    memset(line,0,strlen(line));
-    memset(name,0,strlen(name));
-    ret = fgets(line,MAX_ASCII,srcfile);
+    memset(line,0,strlen((char *)line) + 2);
+    memset(name,0,strlen((char *)name) + 2);
+    ret = fgets((char *)line,MAX_ASCII,srcfile);
     if(ret == NULL)//遇到了文件末尾，跳出循环
     {
       break;
     }
     //printf("%s",line);
     line_deal(line);
+
+	
+    //printf("%s",path);
+    //printf("%s",name);
+    //printf("%s",name_old);
+    //printf("line:%s",line);
+    //printf("\n");
+	sprintf((char *)buffer,"%d,%d,%d\n",line_cnt,symbol_cnt,dir_cnt);
+	fputs((char *)buffer,logfile);
+	
+	fputs((char *)path,destfile);
+	fputs((char *)name,destfile);
+	fputs((char *)"\n",destfile);
   }
   fclose(srcfile);
-  //fclose(destfile);
-  printf("文件共%d行；错误路径有%d行。\n",line_cnt,error_line_cnt);
+  fclose(destfile);
+  // 只有在文件出现错误的时候才进行输出错误信息
+  if (0 != error_line_cnt)
+  {
+	printf("文件共%d行；错误路径有%d行。\n",line_cnt,error_line_cnt);
+  }
 }
+
+void demo()
+{
+	printf("使用方法为“tree2path-c.exe src_tree.txt  dst_path.txt”");
+}
+
 
 int main(int argc, char *argv[])
 {
-//	if(argc <= 1)
-//	{
-//		printf("no file to input,program will exit.\n");
-//		return 0;
-//	}
-  if (argc >= 3)
+	if(argc <= 1)
+	{
+		demo();
+	//	return 0;
+	}
+  /*if (argc >= 3)
   {
-    if (strcmp("linux",argv[2]))
+    if (strcmp("linux",argv[3]))
     {
       symbol_cnt_per_change_dir = LINUX_SYMBOL_CNT_PER_CHANGE_DIR;
     }
-  }
-  file_deal(argv[1]);
-  //system("pause");
+  }*/
+  printf("程序正常启动。\r\n");
+  strcpy((char*)srcfile_str,argv[1]);
+  strcpy((char*)destfile_str,argv[2]);
+  file_deal(srcfile_str,destfile_str);
+  //file_deal((unsigned char*)"3478_tree.txt",(unsigned char*)"3478_path.txt");
+  file_deal((unsigned char *)argv[1],(unsigned char *)argv[2]);
+  printf("程序正常结束。\r\n");
+  system("pause");
   return 0;
 
 }
